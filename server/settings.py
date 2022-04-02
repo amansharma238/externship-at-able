0e7117d98c34aa7fe75cc84394d9f3d07d1f95f2
AmanSharma <amansharma122000@gmail.com>
2022-04-02 18:32:13 +0530

    added procfile

--- server/settings.py
+++ server/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "api.apps.ApiConfig",
     "rest_framework",
     "django_filters",
+    'storages'
 ]
 
 AUTH_USER_MODEL = "authentication.SalesUser"
@@ -142,3 +143,17 @@ STATICFILES_DIRS = [BASE_DIR / "authentication/static"]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_REGION_NAME = 'us-east-2'
+AWS_S3_ADDRESSING_STYLE = 'virtual'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
